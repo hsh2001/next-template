a396df4ecec1b355e65a1534b9779e936337453f
Seung-hyun Hwang <dev@hsh.kr>
2020-11-02 12:18:53 +0900

    Update index.test.tsx

--- __test__/pages/index.test.tsx
+++ __test__/pages/index.test.tsx
@@ -5,10 +5,4 @@ describe('Render Index component', () => {
   it('Render Index component without error', () => {
     shallow(<Index />);
   });
-
-  it('Render Title correctly', () => {
-    const component = shallow(<Index />);
-    expect(component.find('h1').text().trim()).toBe('hello world..!');
-    expect(component.find('p').text().trim()).toBe('Content here');
-  });
 });
