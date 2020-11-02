8459d428c3e03d7a68497a94e6655ba751466200
Seung-hyun Hwang <dev@hsh.kr>
2020-11-02 12:17:35 +0900

    Create Layout.test.tsx

---
+++ __test__/components/Layout.test.tsx
@@ -0,0 +1,8 @@
+import { shallow } from 'enzyme';
+import Layout from '../../components/Layout';
+
+describe('Render Layout component', () => {
+  it('Render Layout component without error', () => {
+    shallow(<Layout />);
+  });
+});
