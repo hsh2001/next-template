a48bb20318323876d9934729bc05d032e3ecb2ae
Asher Hwang <dev@hsh.kr>
2021-12-18 16:20:08 +0900

    Remove e2e test

--- qawolf/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { launch } from 'qawolf';
-
-const { URL } = process.env;
-
-test('visit site', async () => {
-  const { context } = await launch();
-  const page = await context.newPage();
-  await page.goto(String(URL));
-});
